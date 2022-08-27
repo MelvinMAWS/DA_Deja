ed4fd23ab3e19cf02bfad2be0e5922084bb3d435
cooki <melvinang2104@gmail.com>
2022-08-27 20:21:37 +0800

    v1.1 - added comments to testmain.py

--- testmain.py
+++ testmain.py
@@ -4,7 +4,7 @@ import main
 class TestProgram(unittest.TestCase):
     def testStatusCode(self):
 
-        self.assertEqual(main.r.status_code, 200)
+        self.assertEqual(main.r.status_code, 200) #test if status code is 200
 
 if __name__ == '__main__':
     unittest.main()
